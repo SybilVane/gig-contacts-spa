98d244333488c98eeb7c0d99798f1200b0482b32
Sybil <vanessa.zgn@gmail.com>
2024-11-15 20:00:50 +0100

    Added index and app styles. Reset css styles. Created App header, added gig logo, font, favicon.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import React from "react";
 import "./App.css";
+import logo from "./assets/logo.svg";
 
-function App() {
-  return <div>Welcome</div>;
-}
+const App: React.FC = () => {
+  return (
+    <div className="app">
+      <article className="app__container">
+        <header className="app__header">
+          <img src={logo} alt="Gig Contacts" className="app__header-img" />
+          <h1 className="app__header-title">Contacts</h1>
+        </header>
+      </article>
+    </div>
+  );
+};
 
 export default App;
